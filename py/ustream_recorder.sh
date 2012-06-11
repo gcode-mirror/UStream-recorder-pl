8eb2dcf41b2b25018c52024dde5be40a14610ca7
info <info@tknr.com>
2012-06-11 14:20:48 +0000

    perl version is old. use python version.

--- py/ustream_recorder.sh
+++ py/ustream_recorder.sh
@@ -3,7 +3,7 @@ DIR=`pwd`
 cd "${DIR}"
 for CHANNEL in `cat ${DIR}channel.txt | tr -d '\r' | grep -v "^#" | sed -e "s/http:\/\/www\.ustream\.tv\/channel\///g"` ;
 do
-	COMMAND="perl ustream_recorder.pl http://www.ustream.tv/channel/${CHANNEL} ${DIR}${CHANNEL} 43200"
+	COMMAND="python ustreamRTMPDump.py http://www.ustream.tv/channel/${CHANNEL} ${DIR}/${CHANNEL} 43200"
 	LOG="ustroku_${CHANNEL}.log"
 
 	if [ `ps -ef | grep "$COMMAND" | grep -v grep| wc -l` -gt 0 ] ; then
